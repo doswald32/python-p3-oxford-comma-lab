1ecdc04480ff95599e8b4be9d50dc9ec56880070
doswald32 <dan.oswald32@gmail.com>
2024-05-20 17:45:13 -0600

    completed lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        new_list = " and ".join(items)
+        return new_list
+    else:
+        last_element = items.pop()
+        new_list = ", ".join(items) + ", and " + last_element
+        return new_list
